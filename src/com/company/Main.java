21b6039af0bbba12c4e95e8d88769ff890fe4f33
Ivkoncho <51968910+Ivkoncho@users.noreply.github.com>
2022-02-23 16:24:24 +0200

    HomeWork randomNum

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,20 @@
 package com.company;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Ivo");
-        int number = 6;
-        int numberTwo = 7;
-        System.out.println(number +numberTwo);
-        System.out.println(number -numberTwo);
-        System.out.println(number * numberTwo);
-        System.out.println(number / numberTwo);
-        System.out.println(number % numberTwo);
+       System.out.println("Ivo's Homework");
+       System.out.println();
 
-        if (number>numberTwo){
-            System.out.println("That is true");
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 9 + 1);
+        System.out.println("randomNum is - " + randomNum);
+
+        if (randomNum> 5){
+            System.out.println("It is greater then 5");
         }
-        else System.out.println("That is wrong");
-        float price = 2.57F;
-        boolean bool = true;
-        double priceTwo = 2.965;
-
-        System.out.println(price);
-        System.out.println(priceTwo);
-        System.out.println();
-        System.out.println(bool);
-
-        String name = "Tomy";
-        name = "Ivo";
-
-        System.out.println(name);
-
-        double a = 3.5;
-        double b = 2.0;
-        a++;
-        b--;
-        System.out.println(a);
-        System.out.println(b);
 
     }
 }
